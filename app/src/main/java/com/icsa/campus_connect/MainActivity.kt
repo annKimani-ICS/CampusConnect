23db12a0a39b0ee0d1165c3b6084a6cdca86373f
annkimani <ann.wangari@strathmore.edu>
2024-11-17 15:08:22 +0300

    Test for database creation and its file path. The file path for campus_connect.db is incorrect.

--- app/src/main/java/com/icsa/campus_connect/MainActivity.kt
+++ app/src/main/java/com/icsa/campus_connect/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.icsa.campus_connect
 
+import android.content.ContentValues
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,18 +11,45 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.icsa.campus_connect.database.DatabaseHelper
 import com.icsa.campus_connect.ui.theme.Campus_connectTheme
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.text.style.TextDirection.Companion.Content
+import java.io.File
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         setContent {
             Campus_connectTheme {
                 Greeting("Campus Connect") // Update the greeting argument here
             }
         }
+
+        val dbHelper = DatabaseHelper(this)
+        val db = dbHelper.writableDatabase
+
+        // Check if the database is open
+        if (db.isOpen) {
+            Log.d("DatabaseTest", "Database opened successfully.")
+        } else {
+            Log.e("DatabaseTest", "Failed to open database.")
+        }
+
+        // Get the database path
+        val databaseName = "campus_connect.db"
+        val databasePath = getDatabasePath(databaseName)
+
+        Log.d("Database Path", "Database is stored at: $databasePath")
+
     }
+
 }
 
 @Composable
@@ -37,3 +67,4 @@ fun GreetingPreview() {
         Greeting("Campus Connect") // Updated preview message
     }
 }
+
