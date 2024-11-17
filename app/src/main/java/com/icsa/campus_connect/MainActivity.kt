e17b776995499a317ad0ac2e670994d470e1b7c3
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2024-11-17 14:21:32 +0300

    Optimizing imports

--- app/src/main/java/com/icsa/campus_connect/MainActivity.kt
+++ app/src/main/java/com/icsa/campus_connect/MainActivity.kt
@@ -3,7 +3,6 @@ package com.icsa.campus_connect
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
