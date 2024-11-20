d28f495a49a064add83492bfaed34952e97b514d
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2024-11-20 16:39:58 +0300

    Created XML file for Layout to test calendar integration

--- app/src/main/java/com/icsa/campus_connect/CalendarIntegrationTestActivity.kt
+++ app/src/main/java/com/icsa/campus_connect/CalendarIntegrationTestActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.provider.CalendarContract
 import android.widget.Button
 import android.widget.EditText
-import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
