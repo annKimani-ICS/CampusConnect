0b6f47f5bd44de96978922e88d63d1ad4e26b6f0
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2024-11-23 17:00:31 +0300

    Fix: Optimizing imports and removing unused code

--- app/src/main/java/com/icsa/campus_connect/MainActivity.kt
+++ app/src/main/java/com/icsa/campus_connect/MainActivity.kt
@@ -3,7 +3,6 @@ package com.icsa.campus_connect
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,11 +15,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        setContent {
-//            Campus_connectTheme {
-//                Greeting("Campus Connect") // Update the greeting argument here
-//            }
-//
 
         val dbHelper = DatabaseHelper(this)
         val db = dbHelper.writableDatabase
